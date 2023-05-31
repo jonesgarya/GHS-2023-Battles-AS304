37382d7386f6ac6d8ee3623a6257d2921fbd8bd9
Shera304 <135157137+Shera304@users.noreply.github.com>
2023-05-31 11:54:25 -0700

    Update Location.java

--- Location.java
+++ Location.java
@@ -4,12 +4,6 @@ public class Location
     public static final int HIT = 1;
     public static final int MISSED = 2;
 
-    private boolean ship = false;public class Location
-{
-    public static final int UNGUESSED = 0;
-    public static final int HIT = 1;
-    public static final int MISSED = 2;
-
     private boolean ship;
     private int status;
     // Location constructor. 
@@ -81,84 +75,6 @@ public class Location
         {
             return false;
         }
-    }
-    
-    // Set the value of whether this location has a ship.
-    public void setShip(boolean val)
-    {
-        ship = val;
-    }
-    
-    // Set the status of this Location.
-    public void setStatus(int status)
-    {
-        this.status = status;
-    }
-    
-    // Get the status of this Location.
-    public int getStatus()
-    {
-        return status;
-    }
-}
-    private int status;
-    // Location constructor. 
-    public Location()
-    {
-        
-        status = UNGUESSED;
-    }
-    
-    // Was this Location a hit?
-    public boolean checkHit()
-    {
-        if(status == HIT)
-        {
-            return true;
-        }
-        return false;
-    }
-    
-    // Was this location a miss?
-    public boolean checkMiss()
-    {
-        if(status == MISSED)
-        {
-            return true;
-        }
-        return false;
-    }
-    
-    // Was this location unguessed?
-    public boolean isUnguessed()
-    {
-        if(status == UNGUESSED)
-        {
-            return true;
-        }
-        return false;
-    }
-    
-    // Mark this location a hit.
-    public void markHit()
-    {
-        status = HIT;
-    }
-    
-    // Mark this location a miss.
-    public void markMiss()
-    {
-        status = MISSED;
-    }
-    
-    // Return whether or not this location has a ship.
-    public boolean hasShip()
-    {
-        if(ship)
-        {
-            return true;
-        }
-        return false;
     }
     
     // Set the value of whether this location has a ship.
